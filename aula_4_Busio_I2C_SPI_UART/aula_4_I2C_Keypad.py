5ab1db5707fb59bea43e500c144b5d9368fd558b
Djair Guilherme <djair.jr@gmail.com>
2024-04-11 11:04:56 -0300

    Update aula_4_I2C_Keypad.py

--- aula_4_Busio_I2C_SPI_UART/aula_4_I2C_Keypad.py
+++ aula_4_Busio_I2C_SPI_UART/aula_4_I2C_Keypad.py
@@ -11,13 +11,17 @@ SCL_Pin = board.GP21
 i2c = busio.I2C (scl=SCL_Pin, sda=SDA_Pin)
 pcf = adafruit_pcf8574.PCF8574(i2c) # Talvez precise adicionar o Address
 
-''' Eu costumo soldar o Keypad direto no PCF8574'''
+mypassword = ''
+
+# Cada Keypad tem sua própria conexão. Cheque sempre o manual
+# Veja que a ordem dos pinos é estranha...
+
 rows = [pcf.get_pin(5), pcf.get_pin(0), pcf.get_pin(1), pcf.get_pin(3)]
 cols = [pcf.get_pin(4), pcf.get_pin(6), pcf.get_pin(2)]
-keys = ((1, 2, 3),
-        (4, 5, 6),
-        (7, 8, 9),
-        ('*', 0, '#'))
+keys = (('1', '2', '3'),
+        ('4', '5', '6'),
+        ('7', '8', '9'),
+        ('*', '0', '#'))
 
 keypad = adafruit_matrixkeypad.Matrix_Keypad(rows, cols, keys)
 
@@ -25,4 +29,14 @@ while True:
     keys = keypad.pressed_keys
     if keys:
         print("Pressionada: ", keys)
+        if keys!='#':
+            mypassword.append (keys)
+        if keys == '*':
+            mypassword = ''
+
+        if mypassword = '1234':
+            print ('Unlocked')
+            
+            break
+                
     time.sleep(0.1)
