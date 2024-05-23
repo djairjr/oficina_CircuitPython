11c4ed58b8a491dd5c231664890f3ce7ac733feb
Djair Guilherme <djair.jr@gmail.com>
2024-05-23 16:41:49 -0300

    Created auxiliary functions file

--- aula_09_Console/aula_09_auxiliary_functions.py
+++ aula_09_Console/aula_09_auxiliary_functions.py
@@ -1,9 +1,10 @@
 '''
-Rotinas auxiliares
+Várias rotinas auxiliares para jogos
 '''
 
-import time, gc
-import board, random
+# GC is a memory manager
+import gc
+import board, random, time
 import neopixel_spi as neopixel
 
 # Treating Joystick
@@ -15,19 +16,23 @@ from simpleio import map_range
 from tile_framebuf import TileFramebuffer
 spi = board.SPI()
 
+# Setting up the Neopixel Panel
 pixel_pin = board.D10
 pixel_width = 32
 pixel_height = 8
 num_tiles = 2
 num_pixels = pixel_width * pixel_height * num_tiles
 
+# Prepare Joystick in Analog Pins
 joystick_x = AnalogIn(board.A0)
 joystick_y = AnalogIn(board.A1)
 
+# Prepare Trigger Switch 
 trigger = DigitalInOut (board.D2)
 trigger.direction = Direction.INPUT
 trigger.pull = Pull.UP
 
+# Setup Buzzer. Can be any digital Pin
 buzzer = board.D3
 
 pixels = neopixel.NeoPixel_SPI(
@@ -37,21 +42,23 @@ pixels = neopixel.NeoPixel_SPI(
     auto_write=False,
 )
 
+# Prepare PixelFramebuffer Screen
 screen = TileFramebuffer(
     pixels,
     pixel_width,
     pixel_height,
     num_tiles,
-    rotation = 0
+    rotation = 1 # 0 Default. 
 )
 
 def get_joystick():
+    # Returns -1 0 or 1 depending on joystick position
     x_coord = int (map_range (joystick_x.value, 200, 65535, - 2 , 2))
     y_coord = int (map_range (joystick_y.value, 200, 65535, - 2 , 2))
     return x_coord, y_coord
 
 def get_pixel_color(x, y):
-    # Adjusting coordinates based on rotation
+    # Get Pixel color. Deal with screen rotation, because width and height changes...
     if screen.rotation == 1:
         x, y = y, x
         x = screen._width - x - 1
@@ -75,9 +82,37 @@ def check_wall(x, y, wall_color):
     # Check Screen Limits First
     if x < 0 or x >= screen._width or y < 0 or y >= screen._height * screen._tile_num:
         return False
+    # Then check color
     color = get_pixel_color(x, y)
     return color != wall_color
 
 def check_color(x, y, colorcheck):
+    # Only check a color
     color = get_pixel_color(x, y)
-    return color == colorcheck  
+    return color == colorcheck
+
+class Countdown ():
+    """ Countdown Class """
+    
+    def __init__ (self, time_sec):
+        self.time_sec = time_sec
+        self.update()
+        
+    def update(self):
+        current = 0
+        while self.time_sec:
+            now = time.monotonic()       
+            mins, secs = divmod(self.time_sec, 60)
+            timeformat = '{:02d}:{:02d}'.format(mins, secs)
+            print(timeformat, end='\r')
+            # Para atualizar o display HT16K33 substituir
+            # display.print (timeformat)
+            if now >= current + 1:
+                self.time_sec -= 1
+                current = now
+                time.sleep(0.1)
+        
+        print ("KABOOM")
+        time.sleep(0.1)
+        
+        print ("STOP")
